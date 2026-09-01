80ead080053af0934b25da87f57fac467fb9f707
lul_prof <israelmutua539@gmail.com>
2026-09-01 11:46:57 +0300

    echo

--- app/src/data/clientsData.js
+++ app/src/data/clientsData.js
@@ -1,54 +1,152 @@
+/* eslint-disable no-unused-vars */
 // Import images
-import AmazonImage from '../images/brands/amazon_gray.png';
-import SonyImage from '../images/brands/sony_gray.png';
-import AdidasImage from '../images/brands/adidas_gray.png';
-import FilaImage from '../images/brands/fila_gray.png';
-import NBImage from '../images/brands/nb_gray.png';
-import SamsungImage from '../images/brands/samsung_gray.png';
-import CanonImage from '../images/brands/canon_gray.png';
-import PumaImage from '../images/brands/puma_gray.png';
+import aws from '../images/tools/AWS.svg'
+import cplus from '../images/tools/Cplus.svg'
+import docker from '../images/tools/Docker.svg'
+import express from '../images/tools/Express.svg'
+import fastApi from '../images/tools/FastAPI.svg'
+import figma from '../images/tools/Figma.svg'
+import flask from '../images/tools/Flask.svg'
+import flutter from '../images/tools/Flutter.svg'
+import github from '../images/tools/GitHub.svg'
+import heroku from '../images/tools/Heroku.svg'
+import js from '../images/tools/JavaScript.svg'
+import jest from '../images/tools/Jest.svg'
+import kaggle from '../images/tools/Kaggle.svg'
+import materialUi from '../images/tools/Material-UI.svg'
+import mongoDb from '../images/tools/MongoDB.svg'
+import mysql from '../images/tools/MySQL.svg'
+import node from '../images/tools/Node.js.svg'
+import postman from '../images/tools/Postman.svg'
+import react from '../images/tools/React.svg'
+import scikit from '../images/tools/scikit-learn.svg'
+import sqlalchemy from '../images/tools/SQLAlchemy.svg'
+import sqlite from '../images/tools/SQLite.svg'
+import tailwind from '../images/tools/Tailwind.svg'
+import vite from '../images/tools/Vite.js.svg'
 
-export const clientsHeading = 'Some of the brands that trust me';
+
+export const clientsHeading = 'Some of the tools I work with';
 
 export const clientsData = [
 	{
 		id: 1,
-		title: 'Amazon',
-		img: AmazonImage,
+		title: 'AWS',
+		img: aws,
 	},
 	{
 		id: 2,
-		title: 'Sony',
-		img: SonyImage,
+		title: 'CPlus',
+		img: cplus,
 	},
 	{
 		id: 3,
-		title: 'Adidas',
-		img: AdidasImage,
+		title: 'Docker',
+		img: docker,
 	},
 	{
 		id: 4,
-		title: 'FILA',
-		img: FilaImage,
+		title: 'Express',
+		img: express,
 	},
 	{
 		id: 5,
-		title: 'NB',
-		img: NBImage,
+		title: 'Fast Api',
+		img: fastApi,
 	},
 	{
 		id: 6,
-		title: 'SAMSUNG',
-		img: SamsungImage,
+		title: 'Figma',
+		img: figma,
 	},
 	{
 		id: 7,
-		title: 'CANON',
-		img: CanonImage,
+		title: 'Flask',
+		img: flask,
 	},
 	{
 		id: 8,
-		title: 'PUMA',
-		img: PumaImage,
+		title: 'Flutter',
+		img: flutter,
+	},
+	{
+		id: 9,
+		title: 'GitHub',
+		img: github,
+	},
+	{
+		id: 10,
+		title: 'Heroku',
+		img: heroku,
+	},
+	{
+		id: 11,
+		title: 'Javascript',
+		img: js,
+	},
+	{
+		id: 12,
+		title: 'jest',
+		img: jest,
+	},
+	{
+		id: 13,
+		title: 'kaggle',
+		img: kaggle,
+	},
+	{
+		id: 14,
+		title: 'Material Ui',
+		img: materialUi,
+	},
+	{
+		id: 15,
+		title: 'MongoDb',
+		img:mongoDb,
+	},
+	{
+		id: 16,
+		title: 'MySql',
+		img: mysql,
+	},
+	{
+		id: 17,
+		title: 'Node Js',
+		img: node,
+	},
+	{
+		id: 18,
+		title: 'Postman',
+		img: postman,
+	},
+	{
+		id: 19,
+		title: 'React',
+		img:react,
+	},
+	{
+		id: 20,
+		title: 'SciKit',
+		img: scikit,
+	},
+	{
+		id: 21,
+		title: 'SQLAlchemy',
+		img: sqlalchemy,
+	},
+	{
+		id: 22,
+		title: 'Sqlite',
+		img: sqlite,
+	},
+	{
+		id: 23,
+		title: 'Tailwind',
+		img:tailwind,
+	},
+	{
+		id: 24,
+		title: 'Vite',
+		img: vite,
 	},
 ];
